0065b57e973c017b8682bf64cd1554af2cd93724
smnopa <samy.bautista@hotmail.com>
2025-04-07 17:25:35 -0500

    add: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
